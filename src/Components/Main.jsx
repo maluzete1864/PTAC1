0ae00b24f474d8e7b1a7d02620a762bd1bd65541
maluzete1864 <159340154+maluzete1864@users.noreply.github.com>
2024-03-27 10:56:27 -0400

    label, form e etc

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -2,8 +2,11 @@ import { useState } from "react";
 
 export default function Main() {
     const [nome, setNome] = useState("");
+    const [telefone, setTelefone] = useState("");
     return(
         <main>
+            <label>seu nome:</label>
+            <form>
             <input
             type="text"
             name="nome-contato"
@@ -14,6 +17,21 @@ export default function Main() {
               }
             />
             {nome}
+            <label>seu telefone:</label>
+            <input
+            type="tel"
+            name="telefone"
+            id="nome"
+            value={telefone}
+            onChange={
+                (event) => setNome(event.target.value)
+              }
+            />
+              {telefone}
+
+            
+
+            </form>
         </main>
     )
 }
